f1c466d1ec83c7cf8ff396df25ff965dc2a5df88
frog <memopad258@gmail.com>
2019-12-26 03:47:04 +0300

    1

--- P/Program.cs
+++ P/Program.cs
@@ -10,6 +10,41 @@ namespace P
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string type="";
+                Console.WriteLine("Введите название:");
+                string ans=Console.ReadLine();
+                Console.WriteLine("Введите тип :\n 1.Фильм\n 2.Сериал");
+                int typ= int.Parse(Console.ReadLine());
+                switch (typ)
+                {
+                    case 1:
+                        type = "movie";
+                        break;
+                    case 2:
+                        type = "serial";
+                        break;
+                    default:
+                        Console.WriteLine("Введено неверное значение!");
+                        break;
+
+                }
+                ans.Replace(' ', '+');
+                Task<Rezalt[]> f =null;
+                 f=Find.Search.GetResalt(type, ans);
+                foreach(var i in f.Result)
+                {
+                    Console.WriteLine($"Название:{i.title}");
+                    Console.WriteLine($"Популярность:{i.popularity}");
+                    Console.WriteLine($"Рейтинг:{i.vote_average}");
+                    Console.WriteLine($"Описание:{i.overview}");
+                    Console.WriteLine($"Жанр:{i.genre_ids}");
+                    Console.WriteLine($"Дата реализации:{i.release_date}");
+                }
+            }
+                
+                    
         }
     }
 }
---
+++ P/Rezalt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P
+{
+   public class Rezalt
+    {
+        public string poster_path;
+        public bool adult;
+        public string overview;
+        public string release_date;
+        public int[] genre_ids;
+        public int id;
+        public string original_title;
+        public string original_language;
+        public string title;
+        public string backdrop_path;
+        public float popularity;
+        public int vote_count;
+        public bool video;
+        public float vote_average;
+
+
+    }
+}
